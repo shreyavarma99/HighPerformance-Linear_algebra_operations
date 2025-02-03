f237ad5b279932f3dce4dfdd3e6c7c598538ea39
Shreya Varma <shreya36@cs.utexas.edu>
2025-02-03 09:55:55 -0600

    gemm

--- src/shpc_gemm.c
+++ src/shpc_gemm.c
@@ -7,5 +7,18 @@ void shpc_dgemm( int m, int n, int k,
 {
 
     // Your code goes here. 
+    for(int j = 0; j < n; j++) { //columns
+        for(int i = 0; i < m; i++) { //rows
+            double sum = 0.0;
+            for(int l = 0; l < k; l++){
+               
+                int A_index = i * rsA + l * csA;
+                int B_index = l * rsB + j * csB;
+                sum += A[A_index] * B[B_index];
+            }
+            int C_index = i * rsC + j * csC;
+            C[C_index] += sum;
+        }
+    }
 
 }   
